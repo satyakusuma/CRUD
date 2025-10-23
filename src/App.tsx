d3f0ab37ef96d228a62458bd2f7f646ca862b5e6
satyakusuma <satyategar25@gmail.com>
2025-10-23 21:05:23 +0800

    add items list

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react"
+import type { Item } from "./type"
+
+//Data Dummy untuk inisialisasi 
+const initialItems: Item[] = [
+  { id: 1, name:"React", description: "Library UI dari Facebook" },
+  { id: 2, name:"Vue", description: "Framework UI dari Evan You" },
+  { id: 3, name:"Angular", description: "Framework UI dari Google" },
+]
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [items, setItems] = useState<Item[]>(initialItems)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div style={{ padding: '20px' }}>
+      <h1>Daftar Catatan (CRUD DEMO)</h1>
+      
+    </div>
   )
 }
 
---
+++ src/components/ItemList.tsx
@@ -0,0 +1,33 @@
+import type { Item } from '../type'
+
+interface Props {
+    items: Item[];
+    onEdit: (item: Item) => void;
+    onDelete: (id: number) => void;
+}
+
+const ItemList: React.FC<Props> = ({ items, onEdit, onDelete }) => {
+    return (
+        <table>
+            <thead>
+                <tr>
+                    <th>Nama</th>
+                    <th>Deskripsi</th>
+                    <th>Aksi</th>
+                </tr>
+        </thead>
+        <tbody>
+        {items.map((item) => (
+            <tr key={item.id}>
+                <td>{item.name}</td>
+                <td>{item.description}</td>
+                <td>
+                    <button onClick={() => onEdit(item)}>Edit</button>
+                    <button onClick={() => onDelete(item.id)}>Hapus</button>    
+                </td>
+            </tr>
+        ))} 
+        </tbody>
+        </table>
+    );
+}
